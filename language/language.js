1afbeb0e58903d71f1ad53da91196bfdec9e38fb
Arikaim <sales@intersoft.biz>
2021-04-06 20:43:48 +0300

    =

---
+++ language/language.js
@@ -0,0 +1,15 @@
+'use strict';
+
+arikaim.component.onLoaded(function() {  
+    arikaim.ui.button('#language-button',function(element) {
+        $('#language-menu').toggle();
+    });
+    arikaim.ui.button('.language-item',function(element) {
+        var language = $(element).attr('data-value');
+        arikaim.setLanguage(language);
+    });   
+
+    $('#language-menu').mouseleave(function() { 
+        $('#language-menu').fadeOut(600); 
+    });  
+});
