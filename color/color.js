43785482d88d3330dd525ad7d8a91b2f73affa39
Arikaim <info@arikaim.com>
2021-12-06 20:53:26 +0200

    =

---
+++ color/color.js
@@ -0,0 +1,48 @@
+'use strict';
+
+arikaim.component.onLoaded(function(component) {
+
+    component.getButton = function() {
+        return $(component.getElement()).find('.color-pick-button');
+    }
+
+    component.select = function(element) {
+        var bgColor = $(element).attr('bg-color'); 
+        var selected = component.get('background');
+    
+        $(this.getButton()).removeClass(selected).addClass(bgColor);
+        component.set('background',bgColor);
+
+        $(component.getElement()).find('input').val();
+        component.toggleMenu();
+    };
+
+    component.toggleMenu = function() {
+        var menu = $(component.getElement()).find('menu');      
+        if ($(menu).is(":visible") == true) {
+            menu.fadeOut(500);
+        } else {
+            menu.fadeIn(500);
+        }    
+    };
+
+    component.hideMenu = function() {
+        $(component.getElement()).find('menu').fadeOut(500);                            
+    };
+
+    component.init = function() {
+        $(component.getButton()).on('click',function(event) {       
+            component.toggleMenu();
+        });    
+        $(component.getElement()).find('menu').on('mouseleave',function() { 
+            component.hideMenu();
+        });  
+    
+        var colors = $(component.getElement()).find('color');
+        $(colors).on('click',function() {
+            component.select(this);
+        });
+    };
+
+    component.init();
+});
