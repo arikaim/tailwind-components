bc8921dcd4731f51f50daf086576d8e1b99ffc81
Arikaim <info@arikaim.com>
2021-12-06 10:14:59 +0200

    =

--- tooltip/tooltip.js
+++ tooltip/tooltip.js
@@ -5,19 +5,21 @@ arikaim.component.onLoaded(function(component) {
     component.init = function() {
         var items = $('body').find('.show-tooltip');
     
-        $(items).on('mouseover',function() {     
+        $(items).on('mouseover',function() {   
+            $(this).addClass('relative');  
             $(component.getElement()).find('span').html('');      
             var content = $(this).attr('tooltip-content');
             content = (isEmpty(content) == true) ? $(this).attr('title') : content;
             $(component.getElement()).find('span').html(content);
            
             if (isEmpty(content) == false) {
-                $(component.getElement()).detach().prependTo(this);
-                $(component.getElement()).removeClass('.opacity-0').css('opacity',1);   
+                $(component.getElement()).detach().appendTo(this);
+                $(component.getElement()).css('opacity',1);   
             }                
         });
         $(items).on('mouseout',function() {          
-           $(component.getElement()).css('opacity',0);      
+            $(this).removeClass('relative');  
+            $(component.getElement()).css('opacity',0);      
         });
     };
 
