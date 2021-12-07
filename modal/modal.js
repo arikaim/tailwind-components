70c4e05022ecf7f367f6004227d04f80e8873fc3
Arikaim <info@arikaim.com>
2021-12-07 18:04:30 +0200

    =

---
+++ modal/modal.js
@@ -0,0 +1,30 @@
+'use strict';
+
+arikaim.component.onLoaded(function(component) { 
+
+    component.close = function() {             
+        $(this.getElement()).fadeOut(500);
+    };
+
+    component.setContent = function(content) {
+        $(this.getElement()).find('.modal-content').html(content);
+    };
+
+    component.setTitle = function(title) {
+        $(this.getElement()).find('.modal-title').html(title);
+    };
+
+    component.open = function() {     
+        $(this.getElement()).removeClass('hidden').fadeIn(500);  
+    };
+
+    component.init = function() {        
+        var closeButton = $(this.getElement()).find('.modal-close');
+        $(closeButton).off('click');
+        $(closeButton).on('click',function() {
+            component.close();
+        });
+    };
+
+    component.init();
+});
