86bc49fb463f9cee71096b827c2b67fd7ba7b670
Arikaim <info@arikaim.com>
2021-12-29 11:47:46 +0200

    =

--- slider/slider.js
+++ slider/slider.js
@@ -13,11 +13,39 @@ arikaim.component.onLoaded(function(component) {
     component.init = function() {
         var bars = $(component.getElement()).find('.slider-parts');
         $(bars).off();
-        $(bars).on('click',function() {
+        $(bars).on('click',function(e) {
             var value = $(this).attr('bar-value');              
             value = (isEmpty(value) == true) ? component.get('min') : value;
             component.setValue(value);
         });
+
+        $(bars).on('dragenter',function(e) {
+            e.preventDefault();    
+            var barValue = $(this).attr('bar-value');
+            if (isEmpty(barValue) == false) {
+                component.setValue(barValue);
+            }
+        });
+
+        $(bars).on('dragover',function(e) {
+            e.preventDefault();          
+            this.style.cursor = 'pointer';
+            return true;
+        });
+
+        $(this.getToggleElement()).on('dragstart',function(e) {       
+            var target = isObject(e.toElement) ? e.toElement : e.currentTarget;        
+            target.style.cursor = 'move'; 
+
+            this.style.cursor = 'pointer';          
+            return true;
+        }); 
+
+        $(this.getToggleElement()).on('drag',function(e) {
+            e.preventDefault();         
+            this.style.cursor = 'pointer';
+            return true;
+        });       
     }
 
     component.setValue = function(value) {
@@ -26,11 +54,13 @@ arikaim.component.onLoaded(function(component) {
         var min = parseInt(component.get('min'));
         value = (value > max) ? max : value;
         value = (value < min) ? min : value;
-        
+               
         // set width
         $(component.getBarElement()).css('width',value + '%');
         // set toggle button
         $(component.getToggleElement()).css('left',value + '%');
+        
+        component.set('value',value);
     };
 
     component.clear = function() {
