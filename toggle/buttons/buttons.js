5191aadec9b962fdc16b8a4d7e61519c6002c6c9
arikaim <info@arikaim.com>
2022-04-06 06:41:32 -0400

    =

--- toggle/buttons/buttons.js
+++ toggle/buttons/buttons.js
@@ -2,8 +2,21 @@
 
 arikaim.component.onLoaded(function(component) { 
 
-    component.init = function() {
+    component.toggle = function(button) {
+        var buttons = $(component.getElement()).children('toggleButton');
+        $(buttons).removeClass(this.get('active-color'));
+        $(button).addClass(this.get('active-color'));
+        var value = $(button).attr('value');
 
+        this.set('selected',value);
+    };
+
+    component.init = function() {
+        var buttons = $(component.getElement()).children('toggleButton');
+       
+        buttons.on('click',function() {
+            component.toggle(this);
+        });  
     };
 
     component.init();
