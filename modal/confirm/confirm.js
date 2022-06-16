94792c06c63b1bbb79e056fab6272673d3ad4dd0
Arikaim <info@arikaim.com>
2022-06-16 12:48:51 +0300

    =

---
+++ modal/confirm/confirm.js
@@ -0,0 +1,42 @@
+'use strict';
+
+arikaim.component.onLoaded(function(component) { 
+
+    component.close = function() {             
+        $(this.getElement()).removeClass('modal-open');  
+    };
+
+    component.setContent = function(content) {
+        $(this.getElement()).find('.modal-content').html(content);
+    };
+
+    component.setTitle = function(title) {
+        $(this.getElement()).find('.modal-title').html(title);
+    };
+
+    component.open = function(onConfirm, onCancel) {            
+        component.setVar('onConfirm',onConfirm);
+        component.setVar('onCancel',onCancel);
+        
+        $(this.getElement()).addClass('modal-open');  
+    };
+
+    component.init = function() {        
+        var closeButton = $(this.getElement()).find('.modal-cancel-button');
+        var confirmButton = $(this.getElement()).find('.modal-confirm-button');
+
+        $(closeButton).on('click',function() {
+            callFunction(component.getVar('onCancel'));
+            component.close();
+        });
+
+        $(confirmButton).on('click',function() {
+            callFunction(component.getVar('onConfirm'));
+            component.close();
+        });
+    };
+
+    component.init();
+
+    return component;
+});
--- tabs/tabs.js
+++ tabs/tabs.js
@@ -8,12 +8,21 @@ arikaim.component.onLoaded(function(component) {
         $(button).addClass(this.get('active-color'));
         var value = $(button).attr('value');
         var componentName = $(button).attr('component');
-
+        var attributes = $(button)[0].attributes;
+        var params = {};
+        if (isObject(attributes) == true) {          
+            for (var i = 0, len = attributes.length; i < len; i++) {
+                params[attributes[i].name] = attributes[i].value;
+            }
+        }
+       
         this.set('selected',value);
+        
         if (isEmpty(componentName) == false) {
             arikaim.ui.loadComponent({
                 mountTo: $(component.getElement()).attr('tab-content-id'),
-                component: componentName
+                component: componentName,
+                params: params
             });
         }
     };
@@ -21,6 +30,7 @@ arikaim.component.onLoaded(function(component) {
     component.init = function() {
         var buttons = $(component.getElement()).children('tabItem');
         buttons.on('click',function() {
+            console.log(this);
             component.toggle(this);
         });  
     };
