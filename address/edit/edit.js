67c7dd12bd0bf31610b0b7ae41ed62549076d894
Arikaim <info@arikaim.com>
2022-10-16 15:30:34 +0300

    =

--- address/edit/edit.js
+++ address/edit/edit.js
@@ -2,4 +2,18 @@
 
 arikaim.component.onLoaded(function() {
     arikaim.ui.form.addRules('#address_form',{});
+
+    $('#country_dropdown').dropdown({
+        onChange: function(value, text, el) {          
+            var code = $(el).attr('country-code');
+            arikaim.ui.loadComponent({
+                mountTo: 'state_content',
+                component: 'tailwind~address.state',
+                params: {
+                    country_code: code,
+                    class: 'mini'
+                }
+            });
+        }
+    });
 });
